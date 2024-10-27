90338acf415c1ccf1c035469246232ccb79e04e2
gabrielnm123 <gabrielnmatos96@gmail.com>
2024-10-27 17:36:28 -0300

    substituir charAt pra logica de object

--- src/components/collaborator/Modulos/Capacita/CreateFicha.tsx
+++ src/components/collaborator/Modulos/Capacita/CreateFicha.tsx
@@ -45,15 +45,15 @@ const CreateFicha: React.FC<createFichaProps> = (props) => {
     if (typeof cpf === 'string') { cpf = cpf.replace(/\D/g, '') } else return false;
     if (typeof cpf === 'string' && (cpf.length !== 11 || /(\d)\1{10}/.test(cpf))) return false;
     let soma = 0;
-    for (let i = 0; i < 9; i++) soma += parseInt(cpf.charAt(i)) * (10 - i);
+    for (let i = 0; i < 9; i++) soma += parseInt(cpf[i]) * (10 - i);
     let resto = 11 - (soma % 11);
     if (resto === 10 || resto === 11) resto = 0;
-    if (resto !== parseInt(cpf.charAt(9))) return false;
+    if (resto !== parseInt(cpf[9])) return false;
     soma = 0;
-    for (let i = 0; i < 10; i++) soma += parseInt(cpf.charAt(i)) * (11 - i);
+    for (let i = 0; i < 10; i++) soma += parseInt(cpf[i]) * (11 - i);
     resto = 11 - (soma % 11);
     if (resto === 10 || resto === 11) resto = 0;
-    return resto === parseInt(cpf.charAt(10));
+    return resto === parseInt(cpf[10]);
   };
 
   const isValidCNPJ = (cnpj: string) => {
@@ -66,21 +66,21 @@ const CreateFicha: React.FC<createFichaProps> = (props) => {
       let soma = 0;
       let pos = tamanho - 7;
       for (let i = tamanho; i >= 1; i--) {
-        soma += parseInt(numeros.charAt(tamanho - i)) * pos--;
+        soma += parseInt(numeros[tamanho - i]) * pos--;
         if (pos < 2) pos = 9;
       }
       let resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
-      if (resultado !== parseInt(digitos.charAt(0))) return false;
+      if (resultado !== parseInt(digitos[0])) return false;
       tamanho = tamanho + 1;
       numeros = cnpj.substring(0, tamanho);
       soma = 0;
       pos = tamanho - 7;
       for (let i = tamanho; i >= 1; i--) {
-        soma += parseInt(numeros.charAt(tamanho - i)) * pos--;
+        soma += parseInt(numeros[tamanho - i]) * pos--;
         if (pos < 2) pos = 9;
       }
       resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
-      return resultado === parseInt(digitos.charAt(1));
+      return resultado === parseInt(digitos[1]);
     } else return false
   };
 
