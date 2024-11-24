b947aaf39f7ec995a73b33e535ccad969e41e1fe
gabrielnm123 <gabrielnmatos96@gmail.com>
2024-11-24 17:29:43 -0300

    adicionado Spin de carregamento no ultimo modulo, até o momento

--- src/components/collaborator/Modulos/User/ChangeRegistration.tsx
+++ src/components/collaborator/Modulos/User/ChangeRegistration.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Form, Input, Button, message, Typography } from 'antd';
+import { Form, Input, Button, message, Typography, Spin } from 'antd';
 import axiosInstance from '../../services/axiosInstance';
 
 interface FormValues {
@@ -119,84 +119,86 @@ const ChangeRegistration: React.FC = () => {
   };
 
   return (
-    <Form
-      form={form}
-      onFinish={onFinish}
-      layout="vertical"
-    >
-      <Form.Item
-        label="E-mail"
-        name="email"
-        rules={[{ required: true, message: 'Por favor, insira seu e-mail!' }, { type: 'email', message: 'Por favor, insira um e-mail válido!' }]}
+    <Spin spinning={getLoading} tip='Carregando...'>
+      <Form
+        form={form}
+        onFinish={onFinish}
+        layout="vertical"
       >
-        <Input placeholder="E-mail" />
-      </Form.Item>
-      <Form.Item
-        label="Primeiro Nome"
-        name="first_name"
-        rules={[{ required: true, message: 'Por favor, insira seu primeiro nome!' }]}
-      >
-        <Input placeholder="Primeiro Nome" onChange={(e) => form.setFieldsValue({ first_name: e.target.value.toUpperCase() })} />
-      </Form.Item>
-      <Form.Item
-        label="Sobrenome"
-        name="last_name"
-        rules={[{ required: true, message: 'Por favor, insira seu sobrenome!' }]}
-      >
-        <Input placeholder="Sobrenome" onChange={(e) => form.setFieldsValue({ last_name: e.target.value.toUpperCase() })} />
-      </Form.Item>
-      <Form.Item
-        label="Senha Atual"
-        name="currentPassword"
-        rules={[{ required: true, message: 'Por favor, insira sua senha atual!' }]}
-      >
-        <Input.Password placeholder="Senha Atual" />
-      </Form.Item>
-      <Text strong style={{ marginBottom: '8px', display: 'block' }}>
-        Regras para a nova senha (opcional):
-      </Text>
-      <Text style={{ marginBottom: '16px', display: 'block' }}>
-        A senha deve conter pelo menos 8 caracteres, incluindo uma letra maiúscula, uma letra minúscula, um número e um caractere especial. Caracteres especiais permitidos: #@!$%^&*()-_+={ }[]:;"'&lt;&gt;,.?/\\|~`.
-      </Text>
-      <Form.Item
-        label="Nova Senha"
-        name="password"
-        rules={[{ validator: validatePassword }]}
-        hasFeedback
-      >
-        <Input.Password
-          placeholder="Nova senha (opcional)"
-          onChange={(e) => {
-            form.setFieldsValue({ password: e.target.value });
-            form.validateFields(['password']);
-          }}
-        />
-      </Form.Item>
-      {getPasswordMessage && <Text type="danger">{getPasswordMessage}</Text>}
-      <Form.Item
-        label="Confirme a Nova Senha"
-        name="confirmPassword"
-        dependencies={['password']}
-        hasFeedback
-        rules={[
-          ({ getFieldValue }) => ({
-            validator(_, value) {
-              if (!value || getFieldValue('password') === value) {
-                return Promise.resolve();
-              }
-              return Promise.reject(new Error('As senhas não coincidem!'));
-            },
-          }),
-        ]}
-      >
-        <Input.Password placeholder="Confirme a nova senha (se preenchida)" />
-      </Form.Item>
-      <Form.Item>
-        <Button type="primary" htmlType="submit" loading={getLoading}>
-          Alterar Cadastro
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item
+          label="E-mail"
+          name="email"
+          rules={[{ required: true, message: 'Por favor, insira seu e-mail!' }, { type: 'email', message: 'Por favor, insira um e-mail válido!' }]}
+        >
+          <Input placeholder="E-mail" />
+        </Form.Item>
+        <Form.Item
+          label="Primeiro Nome"
+          name="first_name"
+          rules={[{ required: true, message: 'Por favor, insira seu primeiro nome!' }]}
+        >
+          <Input placeholder="Primeiro Nome" onChange={(e) => form.setFieldsValue({ first_name: e.target.value.toUpperCase() })} />
+        </Form.Item>
+        <Form.Item
+          label="Sobrenome"
+          name="last_name"
+          rules={[{ required: true, message: 'Por favor, insira seu sobrenome!' }]}
+        >
+          <Input placeholder="Sobrenome" onChange={(e) => form.setFieldsValue({ last_name: e.target.value.toUpperCase() })} />
+        </Form.Item>
+        <Form.Item
+          label="Senha Atual"
+          name="currentPassword"
+          rules={[{ required: true, message: 'Por favor, insira sua senha atual!' }]}
+        >
+          <Input.Password placeholder="Senha Atual" />
+        </Form.Item>
+        <Text strong style={{ marginBottom: '8px', display: 'block' }}>
+          Regras para a nova senha (opcional):
+        </Text>
+        <Text style={{ marginBottom: '16px', display: 'block' }}>
+          A senha deve conter pelo menos 8 caracteres, incluindo uma letra maiúscula, uma letra minúscula, um número e um caractere especial. Caracteres especiais permitidos: #@!$%^&*()-_+={ }[]:;"'&lt;&gt;,.?/\\|~`.
+        </Text>
+        <Form.Item
+          label="Nova Senha"
+          name="password"
+          rules={[{ validator: validatePassword }]}
+          hasFeedback
+        >
+          <Input.Password
+            placeholder="Nova senha (opcional)"
+            onChange={(e) => {
+              form.setFieldsValue({ password: e.target.value });
+              form.validateFields(['password']);
+            }}
+          />
+        </Form.Item>
+        {getPasswordMessage && <Text type="danger">{getPasswordMessage}</Text>}
+        <Form.Item
+          label="Confirme a Nova Senha"
+          name="confirmPassword"
+          dependencies={['password']}
+          hasFeedback
+          rules={[
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue('password') === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject(new Error('As senhas não coincidem!'));
+              },
+            }),
+          ]}
+        >
+          <Input.Password placeholder="Confirme a nova senha (se preenchida)" />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" loading={getLoading}>
+            Alterar Cadastro
+          </Button>
+        </Form.Item>
+      </Form>
+    </Spin>
   );
 };
 
